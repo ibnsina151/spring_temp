282d4c5448a1b114f77e88dee2ad02a0cdbd1a66
ibnsina151 <ibnsinam12@gmail.com>
2020-12-21 20:22:47 +0600

    JWT Token init sucesfully

---
+++ src/main/java/com/example/demo/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.demo.utils;
+
+public interface Constants {
+
+    String BEARER = "Bearer ";
+
+}
--- src/main/java/com/example/demo/utils/SecurityConfiguration.java
+++ src/main/java/com/example/demo/utils/SecurityConfiguration.java
@@ -7,14 +7,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JWTRequestFilter mJWTRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,7 +30,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(mJWTRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
